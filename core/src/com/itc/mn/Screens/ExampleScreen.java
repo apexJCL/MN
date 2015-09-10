cc116c139eda7f5d58836458935607aa2ccb627c
José Carlos López <zero.chuck@gmail.com>
2015-09-10 18:29:07 -0500

    Update

--- core/src/com/itc/mn/Screens/ExampleScreen.java
+++ core/src/com/itc/mn/Screens/ExampleScreen.java
@@ -3,8 +3,11 @@ package com.itc.mn.Screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.itc.mn.Misc.Const;
@@ -18,13 +21,19 @@ public class ExampleScreen implements Screen {
     private Stage stage;
     private Game game;
     private FitViewport viewport;
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
 
     public ExampleScreen(Game game){
         // Sólo para tener una referencia al manejador de pantallas
         this.game = game;
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
         // Creamos las cosas para renderizar
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Const.WIDTH, Const.HEIGHT);
+        camera.position.set(-500, -2500, 0);
+        camera.update();
         // Definimos el viewport
         viewport = new FitViewport(Const.WIDTH, Const.HEIGHT);
         // Definimos el Stage para entradas táctiles
@@ -45,11 +54,16 @@ public class ExampleScreen implements Screen {
         // Actualizamos el Stage
         stage.act();
         stage.draw();
+        // Renderizamos con el shapeRenderer
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.circle(100, 100, 100);
+        shapeRenderer.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
