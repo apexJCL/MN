ab34be932d4c428c59b1a669915de922dc52065d
José Carlos López <zero.chuck@gmail.com>
2015-10-05 09:30:45 -0500

    LOL YOLO

---
+++ core/src/com/itc/mn/Cosas/VentanaCarga.java
@@ -0,0 +1,19 @@
+package com.itc.mn.Cosas;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.kotcrab.vis.ui.widget.VisWindow;
+
+/**
+ * Created by zero_ on 05/10/2015.
+ */
+public class VentanaCarga extends VisWindow {
+
+    public VentanaCarga(String title) {
+        super(title);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
