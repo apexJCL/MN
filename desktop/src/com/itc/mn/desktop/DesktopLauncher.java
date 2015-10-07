c233c25f304714e23ba47b54b314544320a3b5ca
José Carlos López <zero.chuck@gmail.com>
2015-10-07 08:38:08 -0500

    Actualizacion
    Corregido errores.

--- desktop/src/com/itc/mn/desktop/DesktopLauncher.java
+++ desktop/src/com/itc/mn/desktop/DesktopLauncher.java
@@ -1,11 +1,9 @@
 package com.itc.mn.desktop;
 
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.itc.mn.MainGame;
-import com.itc.mn.Cosas.Const;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
